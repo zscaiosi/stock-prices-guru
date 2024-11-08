b803a6ad6a6db18e13bd1117ebffdd03b962aaad
zscaiosi <zscaio.si@gmail.com>
2024-11-08 13:44:37 -0300

    feature: implementa primeiros controllers mockados;

--- src/app.py
+++ src/app.py
@@ -1,8 +1,12 @@
 from flask import Flask, request
+from application.commands.save_stocks_data_command import SaveStocksDataCommand
+from application.commands.dtos import save_stocks_data_dto
 
 app = Flask(__name__)
 
-base_route = "/stocks-prices"
+base_route = "/stocks-guru"
+
+save_stocks_command = SaveStocksDataCommand(None)
 
 @app.route(base_route + "/login", methods=["POST"])
 def post_login():
@@ -11,3 +15,15 @@ def post_login():
   print(request.json.get('username'))
 
   return { "jwt": "" }
+
+@app.route(base_route + "/stocks/data", methods=["POST"])
+def post_save_stocks_data():
+  print("Request Data:")
+  print(request.json)
+  save_stocks_command.handle(save_stocks_data_dto.SaveStocksDataDto(
+    request.json.get("identifier"),
+    request.json.get("price"),
+    request.json.get("utc_date_time")
+  ))
+
+  return { "success": True }
